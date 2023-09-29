61476593c32b1093e4fbcc3536499c5e9d3314b9
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2023-09-29 19:16:16 -0300

    Chore: Initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,54 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'heading': ['var(--beaufort-for-lol)']
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+    colors: {
+      yellow: {
+        600: '#F4C258',
+        700: '#C89B3C'
+      },
+      grey: {
+        50: '#F9FAFB',
+        100: '#F3F4F6',
+        200: '#E5E7EB',
+        300: '#D1D5DB',
+        400: '#9CA3AF',
+        500: '#6B7280',
+        600: '#4B5563',
+        700: '#374151',
+        800: '#1F2937',
+        900: '#111827',
+        950: '#030712'
+      },
+      black: '#020912',
+      white: '#FFFFFF'
+    },
+    spacing: {
+      '1': '4px',
+      '2': '8px',
+      '3': '12px',
+      '4': '16px',
+      '5': '20px',
+      '6': '24px',
+      '7': '28px',
+      '8': '32px',
+      '10': '40px',
+      '12': '48px',
+      '16': '64px',
+      '20': '80px',
+      '24': '96px',
+      '32': '128px',
+      '40': '160px',
+      '48': '192px'
     },
   },
   plugins: [],
